e3ccbc4b191e65211f89ec9a964f50f7959c9e4b
Su Myat Noe Oo <scm.sumyatnoeoo@gmail.com>
2020-12-27 14:52:59 +0630

    GitHub - Top Navs complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Tailwind CSS v1.x - Examples of Web Pages' ,
+    title: 'Tailwind CSS' ,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
